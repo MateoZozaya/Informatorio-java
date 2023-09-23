9ee11247d1374003c51e892d7983474b33e7158d
MateoZozaya <mateozozaya19@gmail.com>
2023-09-23 20:09:05 -0300

    Ejercicio 1

---
+++ InvestigacionOperativa/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*Suma de dos números: Se le solicita que guarden dos números enteros y los
+sumen.El resultado mostrarlo por pantalla*/
+
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        int num1 =5;
+        int num2 =6;
+        int suma  = num1 + num2;
+
+        System.out.println("El resultado de sumar " + num1 + " y " + num2 + " es " + suma);
+
+    }
+}
---
+++ InvestigacionOperativa/Ejercicio2.java
@@ -0,0 +1,19 @@
+/*Suma de dos números bis: Se les solicita que guarden dos números enteros y los
+sumen. El resultado guardarlo, mostrarlo por pantalla junto con un mensaje que
+indique si se trata de un número par o impar */
+
+
+public class Ejercicio2 {
+    
+    public static void main(String[] args) {
+        
+        int num1 = 7;
+        int num2 = 9;
+        int suma = num1 + num2;
+
+        boolean par;
+        boolean impar;
+
+       
+    }
+}
